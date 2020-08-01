7bb85732afa007ca610754468a7d9079ee186238
Iann Leles <iannleles@outlook.com>
2020-08-01 01:13:58 -0300

    Organizando pastas e definindo regras de negocio

---
+++ Contexto-de-pagamento-OOP-DDD-CQRS/PaymentContext.Domain/Entities/BoletoPayment.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace PaymentContext.Domain.Entities
+{
+
+
+
+    public class BoletoPayment : Payment
+    {
+        public BoletoPayment(
+            string barCode, 
+            string boletoNumber, 
+            DateTime paidDate,
+            DateTime expireDate,
+            decimal total,
+            DateTime totalPaid,
+            string peyer,
+            string document,
+            string address,
+            string email
+            ) : base (
+                paidDate,  
+                expireDate,  
+                total,  
+                totalPaid,  
+                peyer,  
+                document,  
+                address, 
+                email )
+        {
+            BarCode = barCode;
+            BoletoNumber = boletoNumber;
+        }
+
+    public string BarCode { get; private set; }
+
+    public string BoletoNumber { get; private set; }
+
+} 
+    
+}
---
+++ PaymentContext.Tests/Entities/StudentsTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student("Iann", "Leles", "1234561231321", "Hello@iann.com");
+            student.AddSubscription(subscription);
+        }
+    }
+}
